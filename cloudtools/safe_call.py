fab85b96e7e292d6b48e9daac476b50e3b787463
Daniel King <daniel.zidan.king@gmail.com>
2018-09-28 13:45:52 -0400

    simplify and make correct safe_call

--- cloudtools/safe_call.py
+++ cloudtools/safe_call.py
@@ -1,19 +1,16 @@
 import subprocess as sp
 import sys
 
-if sys.version_info >= (3,7):
-    def safe_call(*args):
-        sp.run(args, capture_output=True, check=True)
+if sys.version_info >= (3,0):
+    def decode(x):
+        return x.decode()
 else:
-    if sys.version_info >= (3,0):
-        def decode(x):
-            return x.decode()
-    else:
-        def decode(x):
-            return x
-    def safe_call(*args):
-        try:
-            sp.check_output(args, stderr=sp.STDOUT)
-        except sp.CalledProcessError as e:
-            print(decode(e.output))
-            raise e
+    def decode(x):
+        return x
+
+def safe_call(*args):
+    try:
+        sp.check_output(args, stderr=sp.STDOUT)
+    except sp.CalledProcessError as e:
+        print(decode(e.output))
+        raise e
